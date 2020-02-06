d11334987615a4217a1d46c10022b55b9b5fa4e0
Adrien Rosi <adrienrosi@gmail.com>
2020-02-06 20:16:05 +0100

    Add tests

--- Model/DataInterface.php
+++ Model/DataInterface.php
@@ -4,28 +4,12 @@ namespace AideTravaux\CoupDePouce\Model;
 
 interface DataInterface
 {
-    /**
-     * Retourne le code travaux Coup de pouce économies d'énergie
-     * @return string
-     */
     public function getCoupDePouceCodeTravaux(): string;
 
-    /**
-     * Retourne la catégorie de ressources selon le dispositif certificats d'économies d'énergie
-     * @return string
-     */
     public function getCategorieCee(): string;
 
-    /**
-     * Retourne la surface d'isolant posé
-     * @return float
-     */
     public function getSurfaceIsolant(): float;
 
-    /**
-     * Retourne le nombre de radiateurs installés
-     * @return int
-     */
     public function getNombreRadiateurs(): int;
 
 }
